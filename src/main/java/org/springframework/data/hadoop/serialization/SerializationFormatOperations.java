93ef2fc7a984923b7946e987993749c2b4ff68d4
asavov <asavov@vmware.com>
2013-01-30 14:26:49 +0200

    Introduce experimental serialization format Operations and Template classes.

---
+++ src/main/java/org/springframework/data/hadoop/serialization/SerializationFormatOperations.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.hadoop.serialization;
+
+import java.io.IOException;
+
+/**
+ * Experimental class by analogy with other <code>xxxOperations</code> classes. Needs to validate if it's applicable and
+ * usable in HDFS serialization context/API.
+ * 
+ * @author Alex Savov
+ */
+public interface SerializationFormatOperations {
+
+	public interface SerializationFormatCallback<T> {
+
+		void doInSerializationFormat(SerializationFormat<T> serializationFormat) throws IOException;
+
+	}
+
+	<T> void execute(String destination, SerializationFormatCallback<T> action) throws IOException;
+
+}
---
+++ src/main/java/org/springframework/data/hadoop/serialization/SerializationFormatTemplate.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.hadoop.serialization;
+
+import static org.apache.commons.io.IOUtils.closeQuietly;
+
+import java.io.IOException;
+
+/**
+ * Experimental class by analogy with other xxxTemplate classes.
+ * 
+ * We'll keep it just to validate if it's applicable and usable in HDFS serialization context.
+ * 
+ * @author Alex Savov
+ */
+public class SerializationFormatTemplate implements SerializationFormatOperations {
+
+	protected SerializationFormatObjectFactory sfObjectFactory;
+	
+	public SerializationFormatTemplate(SerializationFormatObjectFactory sfObjectFactory) {
+		this.sfObjectFactory = sfObjectFactory;
+	}
+	
+	@Override
+	public <T> void execute(String destination, SerializationFormatCallback<T> action) throws IOException {
+		
+		sfObjectFactory.setDestination(destination);
+		
+		@SuppressWarnings("unchecked")
+		SerializationFormat<T> serialization = (SerializationFormat<T>) sfObjectFactory.getObject();
+		
+		try {
+			action.doInSerializationFormat(serialization);
+		} finally {
+			closeQuietly(serialization);
+		}
+	}
+
+}
